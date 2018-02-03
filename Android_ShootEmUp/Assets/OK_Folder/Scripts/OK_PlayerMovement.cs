d1dee70c52e0bc66fde9f69412c912d9dcb6ae9d
Oliver Knight <oliknight95@gmail.com>
2018-02-03 21:16:38 +0000

    Comment Code

--- Android_ShootEmUp/Assets/OK_Folder/Scripts/OK_PlayerMovement.cs
+++ Android_ShootEmUp/Assets/OK_Folder/Scripts/OK_PlayerMovement.cs
@@ -5,40 +5,35 @@ using UnityEngine.AI;
 
 public class OK_PlayerMovement : MonoBehaviour {
 
+    // Variables
     private NavMeshAgent nma_Agent;
     private float fl_Speed;
-    //private Rigidbody rb_Player;
-    private GameObject mGO_Centre;
 
 	// Use this for initialization
 	void Start () {
 
+        // Set variable gameobjects and set a static speed variable
         nma_Agent = GetComponent<NavMeshAgent>();
         fl_Speed = nma_Agent.speed;
-        //rb_Player = GetComponent<Rigidbody>();
-        mGO_Centre = GameObject.Find("Center");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        // The ship is constantly moving, transform.Translate works better than using a rigidbody due to jitters
         transform.Translate(Vector3.forward * Time.deltaTime * fl_Speed);
 
+        // Player Input click raycast formed
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
-
+                // Navmesh agent moves to player clicked position
                 nma_Agent.destination = hit.point;
-                //nma_Agent.nextPosition = mGO_Centre.transform.position;
-
-                
 
             }
-
         }
-
 	}
 }
